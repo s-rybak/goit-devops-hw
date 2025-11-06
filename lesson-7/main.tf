d905dc38f68b1b43df3f348e872076249a388cb7
sgoithw <birch_deck_0s@icloud.com>
2025-11-06 23:41:02 +0100

    Updated readme

--- lesson-7/main.tf
+++ lesson-7/main.tf
@@ -14,39 +14,39 @@ provider "aws" {
   profile = "goithw"
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.eks_cluster_name
-}
+# data "aws_eks_cluster" "eks" {
+#   name = module.eks.eks_cluster_name
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.eks_cluster_name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = module.eks.eks_cluster_name
+# }
 
-resource "helm_release" "django_app" {
-  name       = "django-app"
-  chart      = "./charts/django-app"
-  namespace  = "default"
+# resource "helm_release" "django_app" {
+#   name       = "django-app"
+#   chart      = "./charts/django-app"
+#   namespace  = "default"
 
-  set {
-    name  = "image.repository"
-    value = module.ecr.ecr_repository_url
-  }
+#   set {
+#     name  = "image.repository"
+#     value = module.ecr.ecr_repository_url
+#   }
   
-  set {
-    name  = "image.tag"
-    value = "latest"
-  }
+#   set {
+#     name  = "image.tag"
+#     value = "latest"
+#   }
 
-  depends_on = [module.eks]
-}
+#   depends_on = [module.eks]
+# }
 
 # Підключаємо модуль S3 та DynamoDB
 module "s3_backend" {
