c4f4af392f620252ba35aa0ae4f75376977bb2d8
sgoithw <birch_deck_0s@icloud.com>
2025-11-15 23:21:28 +0100

    Commented modules to start from scratch

--- Progect/main.tf
+++ Progect/main.tf
@@ -14,27 +14,27 @@ provider "aws" {
   profile = "goithw"
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.eks_cluster_name
-}
+# data "aws_eks_cluster" "eks" {
+#   name = module.eks.eks_cluster_name
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.eks_cluster_name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = module.eks.eks_cluster_name
+# }
 
 # Підключаємо модуль S3 та DynamoDB
 module "s3_backend" {
@@ -72,29 +72,29 @@ module "eks" {
 }
 
 #Підключаємо модуль Jenkins
-module "jenkins" {
-  source       = "./modules/jenkins"
-  cluster_name = module.eks.eks_cluster_name
-  jenkins_admin_password = var.jenkins_admin_password
-  github_username = var.github_username
-  github_pat = var.github_pat
-  github_url = var.github_url
-  github_main_branch = var.github_main_branch
-  oidc_provider_arn = module.eks.oidc_provider_arn
-  oidc_provider_url = module.eks.oidc_provider_url
-  providers = {
-    helm = helm
-  }
-}
+# module "jenkins" {
+#   source       = "./modules/jenkins"
+#   cluster_name = module.eks.eks_cluster_name
+#   jenkins_admin_password = var.jenkins_admin_password
+#   github_username = var.github_username
+#   github_pat = var.github_pat
+#   github_url = var.github_url
+#   github_main_branch = var.github_main_branch
+#   oidc_provider_arn = module.eks.oidc_provider_arn
+#   oidc_provider_url = module.eks.oidc_provider_url
+#   providers = {
+#     helm = helm
+#   }
+# }
 
-#Підключаємо модуль Argo CD
-module "argo_cd" {
-  source       = "./modules/argo_cd"
-  namespace    = "argocd"
-  chart_version = "5.46.4"
-  github_username = var.github_username
-  github_pat = var.github_pat
-  github_url = var.github_tf_url
-  github_main_branch = var.github_tf_branch
-  helm_chart_path = var.helm_chart_path
-}
+# #Підключаємо модуль Argo CD
+# module "argo_cd" {
+#   source       = "./modules/argo_cd"
+#   namespace    = "argocd"
+#   chart_version = "5.46.4"
+#   github_username = var.github_username
+#   github_pat = var.github_pat
+#   github_url = var.github_tf_url
+#   github_main_branch = var.github_tf_branch
+#   helm_chart_path = var.helm_chart_path
+# }
